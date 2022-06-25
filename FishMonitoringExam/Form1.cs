81a7aad89f66af3273e283c893728be025b12c9d
riz zar <riazonbin@mail.ru>
2022-06-25 09:48:15 +0300

    Добавьте файлы проекта.

---
+++ FishMonitoringExam/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FishMonitoringExam
+{
+    public partial class FishMonitoring : Form
+    {
+        public FishMonitoring()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_Temps_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && (e.KeyChar != ',') && (e.KeyChar != '-') && (e.KeyChar != (char)8))
+            {
+                e.Handled = true;
+            }
+            if ((e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf(',') > -1))
+            {
+                e.Handled = true;
+            }
+
+
+            if ((e.KeyChar == '-') && ((sender as TextBox).Text.IndexOf('-') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
